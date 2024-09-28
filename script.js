7dc1b19a8095dcd5db1f41887b4693bcfac2ad24
ongkenith <kenith.ong.2023@scis.smu.edu.sg>
2024-09-28 16:39:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-<<<<<<< Updated upstream
-/* Print Navigation Top Bar */
-let nav_bar_contents = 
-`<div id='nav_bar' class="container-fluid py-1 sticky-top">
-    <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
-        <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="index.html">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="projects.html">Projects</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#" onclick="error_alert()">Contact</a>
-            </li>
-            </ul>
-        </div>
-        </div>
-    </nav>
-</div>`;
-=======
 function new_navbar() {
     // Function that creates Navbar based on navbar_contents object modification
     const navbar_contents = {
@@ -31,7 +5,6 @@ function new_navbar() {
         "content2" : {"name" : "Projects", "href_link" : "project.html", "status" : true},
         "content3" : {"name" : "Contact", "href_link" : "#", "status" : false}
     };
->>>>>>> Stashed changes
 
     /* Get the section of Navbar */
     let navbar = document.getElementById('navbar');
